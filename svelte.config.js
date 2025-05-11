7537f82f9aa54d842c2079cfac2de49482f888e8
Mohan Kumar <mohankumarvaddi2004@gmail.com>
2025-05-11 16:41:19 +0530

    changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node'; // Switch to the Node adapter
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(), // Use the Node adapter for server-side deployment
+  }
 };
 
 export default config;
